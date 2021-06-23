cee421a8d0542db2be4f13d431cde0f6bc833365
Mohamed Elhaj <mo.elhaj@outlook.com>
2021-06-23 20:29:49 +0200

    Add files via upload

---
+++ precache-manifest.7f3a32f83b7cc1e3f4a3d58ca70fc416.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59492eff98cf1705f706f79f4dd79fb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09068ed37d4c86474d9c",
+    "url": "/static/css/main.a53f47a3.chunk.css"
+  },
+  {
+    "revision": "e6b419d43b97d31fff2e",
+    "url": "/static/js/2.e5beee80.chunk.js"
+  },
+  {
+    "revision": "c3d52dd675fd79a82511914ca522ab9d",
+    "url": "/static/js/2.e5beee80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09068ed37d4c86474d9c",
+    "url": "/static/js/main.aeb53ca1.chunk.js"
+  },
+  {
+    "revision": "76da655c14c9e4f8c3dc",
+    "url": "/static/js/runtime-main.cfb38967.js"
+  },
+  {
+    "revision": "4a54518868f375d6c7dce67c83427d1e",
+    "url": "/static/media/car.4a545188.JPG"
+  },
+  {
+    "revision": "706bb9e5c7b158ca01d534cf90958aa5",
+    "url": "/static/media/expenses.706bb9e5.jpg"
+  },
+  {
+    "revision": "e2e3ec25ceda3662bb9330e72dd54fa0",
+    "url": "/static/media/game.e2e3ec25.JPG"
+  },
+  {
+    "revision": "d7520f58f1daaad7a979e358adcef438",
+    "url": "/static/media/hrms.d7520f58.jpg"
+  },
+  {
+    "revision": "28095af2dffcc5666d52aad179fec59a",
+    "url": "/static/media/kora.28095af2.jpg"
+  },
+  {
+    "revision": "023eb14a16e895667d867516c2452da0",
+    "url": "/static/media/task.023eb14a.jpg"
+  }
+]);
